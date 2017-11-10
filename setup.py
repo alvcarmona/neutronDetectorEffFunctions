147e86730db588cb42d63ed60aeb91639c5fd8e2
Alvaro Carmona Basanez <alvarocbasanez@ci0011810.esss.lu.se>
2017-11-10 10:52:05 +0100

    fixing

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'neutronDetectorEffFunctions',
-  packages = ['neutronDetectorEffFunctions'], 
+  name = 'neutron_detector_effFunctions',
+  packages = ['neutron_detector_effFunctions'],
   version = '0.1',
   description = 'A library to calculate Neutron detector theoretical efficiency',
   author = 'Alvaro Carmona',
   author_email = 'acarmona@opendeusto.es',
-  url = '', # URL to the github repo
-  download_url = '', 
+  url = 'https://github.com/alvcarmona/neutronDetectorEffFunctions', # URL to the github repo
+  download_url = 'https://github.com/alvcarmona/neutronDetectorEffFunctions/archive/0.1.tar.gz',
   keywords = ['physics', 'neutron', 'science'], 
   classifiers = [],
 )
