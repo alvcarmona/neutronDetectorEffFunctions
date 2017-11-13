7cdbd64baa859e229e5b89b5bf2f6b001a8cdfa7
Alvaro Carmona Basanez <alvarocbasanez@ci0011810.esss.lu.se>
2017-11-13 13:38:09 +0100

    added non code files

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(
   author = 'acarmona',
   author_email = 'acarmona@opendeusto.es',
   url = 'https://github.com/alvcarmona/neutronDetectorEffFunctions', # URL to the github repo
-  download_url = 'https://github.com/alvcarmona/neutronDetectorEffFunctions/archive/0.1.2.tar.gz',
+  download_url = 'https://github.com/alvcarmona/neutronDetectorEffFunctions/archive/0.1.3.tar.gz',
   keywords = ['science'],
   classifiers = [],
+  include_package_data=True
 )
 
