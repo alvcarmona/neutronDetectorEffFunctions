e2f4e63c514cda45432fd9c2b15a5dd78755c4ef
Alvaro Carmona Basanez <alvarocbasanez@ci0011810.esss.lu.se>
2017-12-01 15:05:26 +0100

    renamed data files

--- neutron_detector_eff_functions/Aluminium.py
+++ neutron_detector_eff_functions/Aluminium.py
@@ -42,7 +42,7 @@ def read_cross_section(lambdalist):
     for l in lambdalist:
         lamen.append(((ht ** 2) * 4 * math.pi ** 2) / (2 * nmass * (l[0] ** 2)) * 1e20 * 6.24e18)
 
-    x, y = np.loadtxt(fname=os.path.dirname(os.path.abspath(__file__)) + "/../data/Aluminium/AlCrossSect_(n,g).txt",
+    x, y = np.loadtxt(fname=os.path.dirname(os.path.abspath(__file__)) + "/../data/Aluminium/AlCrossSect_(n,g).py",
                       delimiter=',', unpack=True)
     xlog = []
     ylog = []
