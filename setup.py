c5112395ee1cf2b0385a7fc63634b82c29932331
Alvaro Carmona Basanez <alvarocbasanez@ci0011810.esss.lu.se>
2017-12-01 16:16:39 +0100

    added pacakge init for crossection data

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='neutron_detector_eff_functions',
     packages=['neutron_detector_eff_functions'],
-    version='0.1.7',
+    version='0.1.8',
     description='A library to calculate Neutron detector theoretical efficiency',
     author='acarmona',
     author_email='acarmona@opendeusto.es',
     url='https://github.com/alvcarmona/neutronDetectorEffFunctions',  # URL to the github repo
-    download_url='https://github.com/alvcarmona/neutronDetectorEffFunctions/archive/0.1.7.tar.gz',
+    download_url='https://github.com/alvcarmona/neutronDetectorEffFunctions/archive/0.1.8.tar.gz',
     keywords=['science'],
     install_requires=[
         'numpy',
