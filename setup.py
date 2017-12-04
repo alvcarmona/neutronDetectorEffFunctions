b789b9f2cc02c211b294b3ebb19112279221a15d
Alvaro Carmona Basanez <alvarocbasanez@ci0011810.esss.lu.se>
2017-12-04 10:03:34 +0100

    include subpackages

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='neutron_detector_eff_functions',
-    packages=['neutron_detector_eff_functions'],
-    version='0.1.8',
+    packages=['neutron_detector_eff_functions','neutron_detector_eff_functions.data.B10','neutron_detector_eff_functions.data.B10.10B4C2.20','neutron_detector_eff_functions.data.B10.10B4C2.24'],
+    version='0.1.9',
     description='A library to calculate Neutron detector theoretical efficiency',
     author='acarmona',
     author_email='acarmona@opendeusto.es',
     url='https://github.com/alvcarmona/neutronDetectorEffFunctions',  # URL to the github repo
-    download_url='https://github.com/alvcarmona/neutronDetectorEffFunctions/archive/0.1.8.tar.gz',
+    download_url='https://github.com/alvcarmona/neutronDetectorEffFunctions/archive/0.1.9.tar.gz',
     keywords=['science'],
     install_requires=[
         'numpy',
